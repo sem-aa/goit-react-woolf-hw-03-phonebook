40b0dff3873ab9cc750526a7cde91eb8ac04c9dc
sem-aa <66129330+sem-aa@users.noreply.github.com>
2024-03-30 11:44:07 +0200

    change name files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import ContactForm from './ContactForm';
-import Filter from './Filter';
-import ContactList from './ContactList';
-import Container from './Container';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import Container from './Container/Container';
 
 class App extends Component {
   state = {
